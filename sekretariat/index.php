ae2ef3a9eddaa391318f385a6a221cac2ff36e52
IamYukine <wiktor.wlodarczyk@spoko.pl>
2023-03-16 10:50:52 +0100

    sekretariat plik php

---
+++ sekretariat/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sekretariat</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+ 
+    <main>
+     
+      <h1>Akta Pracownicze</h1>
+ 
+      <hr> 
+      <h3>Dokumenty pracownika</h3>
+      <a href="cv.txt">Pobierz</a>
+      <h1>Liczba zatrudnionych pracowników</h1>
+      <p></p>
+    </main>   
+ 
+g
+ <aside>
+
+
+
+ </aside>
+ 
+ 
+ <footer>
+    Autorem aplikacji jest:uwu
+    <ul>
+        <li>skontaktuj się</li>
+        <li>poznaj naszą firmę</li>
+    </ul>
+ </footer>
+</body>
+</html>
